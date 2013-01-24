0d7601f44fab69e692af5d8ed8162325cf31bcac
Astrid Emde <astrid.emde@wheregroup.com>
2013-01-24 16:49:18 +0000

    moved von trunk

--- data/konvert/postnas_0.6/db_austausch.sql
+++
@@ -1,7 +0,0 @@
-
--- Datenbanken umbenennen.
-
--- Dazu eine andere Datenbank oeffnen und das folgende SQL ausfuehren.
-
-Alter Database alkis06150     rename to  alkis06150_alt;
-Alter Database alkis06150neu  rename to  alkis06150;
