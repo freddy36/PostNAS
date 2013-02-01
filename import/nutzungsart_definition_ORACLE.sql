b553b5423ec006e0cdb038705fbf03894d26eda8
Astrid Emde <astrid.emde@wheregroup.com>
2013-02-01 15:58:00 +0000

    CONSTRAINTS und View noch prüfen

--- import/nutzungsart_definition_ORACLE.sql
+++ import/nutzungsart_definition_ORACLE.sql
@@ -1,86 +1,89 @@
 -- Automatisch mit pg-to-oci_keytables.pl konvertiert.
 ---
 ---
-
-SET client_encoding = 'UTF-8';
+define alkis_epsg=&1
 
 DELETE FROM user_sdo_geom_metadata WHERE upper(table_name)='NUTZUNG_META';
 BEGIN EXECUTE IMMEDIATE 'DROP TABLE NUTZUNG_META CASCADE CONSTRAINTS'; EXCEPTION WHEN OTHERS THEN NULL; END;
 /
 CREATE TABLE NUTZUNG_META (
-	nutz_id           integer NOT NULL,
-	gruppe            character varying(30),
-	source_table      character varying(50),
-	title             character varying(50),
-	fldclass          character varying(30),
-	fldinfo           character varying(30),
-	CONSTRAINT ALKIS_KEY_0 PRIMARY KEY (nutz_id)
+	NUTZ_ID           integer NOT NULL,
+	GRUPPE            varchar2(30),
+	SOURCE_TABLE      varchar2(50),
+	TITLE             varchar2(50),
+	FLDCLASS          varchar2(30),
+	FLDINFO           varchar2(30),
+	CONSTRAINT ALKIS_KEYN_0 PRIMARY KEY (NUTZ_ID)
 );
-COMMENT ON TABLE  nutzung_meta                IS 'Gruppierung und Indizierung der zusammen gefassten Nutzungsarten in der Tabelle "nutzung".';
-COMMENT ON COLUMN nutzung_meta.nutz_id        IS 'Index fuer die Quell-Tabelle bei der Zusammenfassung in der Tabelle "nutzung".';
-COMMENT ON COLUMN nutzung_meta.source_table   IS 'Name der importierten Tabelle aus PostNAS.';
-COMMENT ON COLUMN nutzung_meta.gruppe         IS 'Objektartengruppe, Gruppierung der Nutzungsart.';
-COMMENT ON COLUMN nutzung_meta.title          IS 'Vorzeigbare Bezeichnung der Nutzungsartentabelle.';
-COMMENT ON COLUMN nutzung_meta.fldclass       IS 'Name des Feldes aus "source_table", das in Feld "nutzung.class" kopiert wird.';
-COMMENT ON COLUMN nutzung_meta.fldinfo        IS 'Name des Feldes aus "source_table", das in Feld "nutzung.info" kopiert wird.';
+COMMENT ON TABLE  NUTZUNG_META                IS 'Gruppierung und Indizierung der zusammen gefassten NUTZUNGsarten in der Tabelle "NUTZUNG".';
+COMMENT ON COLUMN NUTZUNG_META.NUTZ_ID        IS 'Index fuer die Quell-Tabelle bei der Zusammenfassung in der Tabelle "NUTZUNG".';
+COMMENT ON COLUMN NUTZUNG_META.SOURCE_TABLE   IS 'Name der importierten Tabelle aus PostNAS.';
+COMMENT ON COLUMN NUTZUNG_META.GRUPPE         IS 'Objektartengruppe, Gruppierung der NUTZUNGsart.';
+COMMENT ON COLUMN NUTZUNG_META.TITLE          IS 'Vorzeigbare Bezeichnung der NUTZUNGsartentabelle.';
+COMMENT ON COLUMN NUTZUNG_META.FLDCLASS       IS 'Name des Feldes aus "source_table", das in Feld "NUTZUNG.CLASS" kopiert wird.';
+COMMENT ON COLUMN NUTZUNG_META.FLDINFO        IS 'Name des Feldes aus "source_table", das in Feld "NUTZUNG.info" kopiert wird.';
 
-DELETE FROM user_sdo_geom_metadata WHERE upper(table_name)='NUTZUNG';
+DELETE FROM user_sdo_geom_METAdata WHERE upper(table_name)='NUTZUNG';
 BEGIN EXECUTE IMMEDIATE 'DROP TABLE NUTZUNG CASCADE CONSTRAINTS'; EXCEPTION WHEN OTHERS THEN NULL; END;
 /
 CREATE TABLE NUTZUNG (
-	gml_id		character(16),
-	beginnt		character(20),
-	nutz_id		integer,
-	class		integer,
-	info		integer,
-	zustand		integer,
-	"name"		varchar2(2047),
-	bezeichnung	varchar2(2047),
-	CONSTRAINT	nutzung_pk      PRIMARY KEY (gml_id, beginnt),
-	CONSTRAINT	nutzung_meta_id FOREIGN KEY (nutz_id)
-		REFERENCES nutzung_meta (nutz_id) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE CASCADE
-)
-WITH (OIDS=FALSE);
-SELECT AddGeometryColumn('nutzung','ora_geometry','25832','POLYGON',2);
-CREATE INDEX ALKIS_KEY_1  ON nutzung (nutz_id, class);
-CREATE INDEX ALKIS_KEY_2 ON NUTZUNG(ORA_GEOMETRY) INDEXTYPE IS MDSYS.SPATIAL_INDEX PARALLEL;
-COMMENT ON TABLE  nutzung             IS 'Zusammenfassung von 26 Tabellen des Objektbereiches "Tatsächliche Nutzung".';
-COMMENT ON COLUMN nutzung.gml_id      IS 'Identifikator, global eindeutig';
-COMMENT ON COLUMN nutzung.nutz_id     IS 'Index fuer die Quell-Tabelle bei der Zusammenfassung in der Tabelle "nutzung".';
-COMMENT ON COLUMN nutzung.class       IS 'Klassifizierung innerhalb der Nutzung. Aus verschiedenen Feldern importiert. Siehe "nutzung_meta.fldclass".';
-COMMENT ON COLUMN nutzung.info        IS 'Weitere verschlüsselte Information zur Nutzung. Aus verschiedenen Feldern importiert. Siehe "nutzung_meta.fldinfo".';
-COMMENT ON COLUMN nutzung.name        IS 'NAM Eigenname';
-COMMENT ON COLUMN nutzung.bezeichnung IS 'weitere unverschlüsselte Information wie Zweitname, Bezeichnung, fachliche Nummerierung usw.';
-COMMENT ON COLUMN nutzung.zustand     IS 'ZUS "Zustand" beschreibt, ob der Abschnitt ungenutzt ist.';
+	GML_ID		character(16),
+	BEGINNT		character(20),
+	NUTZ_ID		integer,
+	CLASS		integer,
+	INFO		integer,
+	ZUSTAND		integer,
+	NAME		varchar2(2047),
+	BEZEICHNUNG	varchar2(2047),
+	CONSTRAINT	NUTZUNG_pk      PRIMARY KEY (GML_ID, BEGINNT)--,
+	--CONSTRAINT	NUTZUNG_META_id FOREIGN KEY (NUTZ_ID)       -- fuer ORACLE noch anpassen
+	--	REFERENCES NUTZUNG_META (NUTZ_ID) MATCH SIMPLE
+	--	ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+--SELECT AddGeometryColumn('NUTZUNG','ora_geometry','25832','POLYGON',2);
+INSERT INTO user_sdo_geom_metadata(table_name,column_name,srid,diminfo) VALUES ('NUTZUNG','ORA_GEOMETRY',&&alkis_epsg,mdsys.sdo_dim_array(mdsys.sdo_dim_element('X',200000,800000,0.001),mdsys.sdo_dim_element('Y',5200000,6100000,0.001)));
+
+CREATE INDEX ALKIS_KEYN_1  ON NUTZUNG (NUTZ_ID, CLASS);
+CREATE INDEX ALKIS_KEYN_2 ON NUTZUNG(ORA_GEOMETRY) INDEXTYPE IS MDSYS.SPATIAL_INDEX PARALLEL;
+COMMENT ON TABLE  NUTZUNG             IS 'Zusammenfassung von 26 Tabellen des Objektbereiches "Tatsächliche NUTZUNG".';
+COMMENT ON COLUMN NUTZUNG.GML_ID      IS 'Identifikator, global eindeutig';
+COMMENT ON COLUMN NUTZUNG.NUTZ_ID     IS 'Index fuer die Quell-Tabelle bei der Zusammenfassung in der Tabelle "NUTZUNG".';
+COMMENT ON COLUMN NUTZUNG.CLASS       IS 'Klassifizierung innerhalb der NUTZUNG. Aus verschiedenen Feldern importiert. Siehe "NUTZUNG_META.fldCLASS".';
+COMMENT ON COLUMN NUTZUNG.INFO        IS 'Weitere verschlüsselte Information zur NUTZUNG. Aus verschiedenen Feldern importiert. Siehe "NUTZUNG_META.fldinfo".';
+COMMENT ON COLUMN NUTZUNG.NAME        IS 'NAM Eigenname';
+COMMENT ON COLUMN NUTZUNG.BEZEICHNUNG IS 'weitere unverschlüsselte Information wie Zweitname, Bezeichnung, fachliche Nummerierung usw.';
+COMMENT ON COLUMN NUTZUNG.ZUSTAND     IS 'ZUS "Zustand" beschreibt, ob der Abschnitt ungenutzt ist.';
 
 DELETE FROM user_sdo_geom_metadata WHERE upper(table_name)='NUTZUNG_CLASS';
 BEGIN EXECUTE IMMEDIATE 'DROP TABLE NUTZUNG_CLASS CASCADE CONSTRAINTS'; EXCEPTION WHEN OTHERS THEN NULL; END;
 /
 CREATE TABLE NUTZUNG_CLASS (
-	nutz_id       integer NOT NULL,
-	class         integer NOT NULL,
-	label         character varying(100),
-	blabla        character varying(1000),
-	CONSTRAINT ALKIS_KEY_3 PRIMARY KEY (nutz_id, class),
-	CONSTRAINT ALKIS_KEY_4 FOREIGN KEY (nutz_id)
-		REFERENCES nutzung_meta (nutz_id) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE CASCADE
+	NUTZ_ID       integer NOT NULL,
+	CLASS         integer NOT NULL,
+	LABEL         varchar2(100),
+	BLABLA        varchar2(1000),
+	CONSTRAINT ALKIS_KEYN_3 PRIMARY KEY (NUTZ_ID, CLASS)--,
+	--CONSTRAINT ALKIS_KEYN_4 FOREIGN KEY (NUTZ_ID)         -- fuer ORACLE noch anpassen
+	--	REFERENCES NUTZUNG_META (nutz_id) MATCH SIMPLE
+	--	ON UPDATE NO ACTION ON DELETE CASCADE
 );
-COMMENT ON TABLE  nutzung_class            IS 'Schlüsseltabelle. Feinere Klassifizierung der zusammen gefassten Nutzungsarten.';
-COMMENT ON COLUMN nutzung_class.nutz_id    IS 'Index fuer die Quell-Tabelle bei der Zusammenfassung in der Tabelle nutzung.';
-COMMENT ON COLUMN nutzung_class.class      IS 'Key, Schlüsselwert.';
-COMMENT ON COLUMN nutzung_class.label      IS 'Entschlüsselung. Art der Nutzung, Dies Feld soll in der Auskunft angezeigt werden.';
-COMMENT ON COLUMN nutzung_class.blabla     IS 'Weitere Erläuterungen und Definitionen dazu.';
-CREATE OR REPLACE VIEW nutzung_mehrfache_gml AS
-	SELECT	a.gml_id, 
-		a.nutz_id,
-		a.beginnt as beginnt1, 
-		b.beginnt as beginnt2
-	FROM nutzung a 
-	JOIN nutzung b 
-	  ON a.gml_id = b.gml_id
-	WHERE a.beginnt < b.beginnt
+COMMENT ON TABLE  NUTZUNG_CLASS            IS 'Schlüsseltabelle. Feinere Klassifizierung der zusammen gefassten NUTZUNGsarten.';
+COMMENT ON COLUMN NUTZUNG_CLASS.NUTZ_ID    IS 'Index fuer die Quell-Tabelle bei der Zusammenfassung in der Tabelle NUTZUNG.';
+COMMENT ON COLUMN NUTZUNG_CLASS.CLASS      IS 'Key, Schlüsselwert.';
+COMMENT ON COLUMN NUTZUNG_CLASS.LABEL      IS 'Entschlüsselung. Art der NUTZUNG, Dies Feld soll in der Auskunft angezeigt werden.';
+COMMENT ON COLUMN NUTZUNG_CLASS.BLABLA     IS 'Weitere Erläuterungen und Definitionen dazu.';
+
+
+CREATE OR REPLACE VIEW NUTZUNG_MEHRFACHE_GML AS
+	SELECT	a.GML_ID, 
+		a.NUTZ_ID,
+		a.BEGINNT as BEGINNT1, 
+		b.BEGINNT as BEGINNT2
+	FROM NUTZUNG a 
+	JOIN NUTZUNG b 
+	  ON a.GML_ID = b.GML_ID
+	WHERE a.BEGINNT < b.BEGINNT
 	;
 purge recyclebin;
 QUIT;
